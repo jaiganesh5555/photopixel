98766f75eaf6822da42f0e9e43c4b99e3a4e233f
jaiganesh5555 <142227919+jaiganesh5555@users.noreply.github.com>
2025-07-07 15:47:20 +0530

    Update config.ts

--- apps/web/app/config.ts
+++ apps/web/app/config.ts
@@ -1,5 +1,5 @@
 // Use localhost for local development
-export const BACKEND_URL = "http://localhost:8080";
+export const BACKEND_URL = "https://photopixel.gjaiganesh.top:8080";
 export const CLOUDFLARE_URL = "https://pub-ef34763ed7604da3af89117f48ad57b4.r2.dev";
 
 
@@ -10,7 +10,7 @@ const isDocker = () => {
 // Use the appropriate backend URL based on the environment
 export const getBackendUrl = () => {
   if (isDocker()) {
-    return "http://backend:8080";
+    return "https://backend:8080";
   }
   return BACKEND_URL;
 };
